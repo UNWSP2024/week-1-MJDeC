fc8038ab0ee866e2a1f60e25204d4aa4b9dc6867
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-05 09:18:20 -0500

    Update program_1.py
    Added my name, date, and title for program as comments. Put in Hello World program.

--- program_1.py
+++ program_1.py
@@ -1,6 +1,9 @@
+# Micah DeCaro
+# 9/5/24
+# Salutations to Earth
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    print('Hello World')
 
 hello_world()
