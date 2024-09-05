043bc53db297544b6647b0df921593300b53cbbf
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-05 09:39:33 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,12 @@
+# Micah DeCaro
+# 9/5/24
+# Excellent Printing of Proverbs 22:29
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+    print(""""“Do you see a man skillful in his work? 
+He will stand before kings;
+he will not stand before obscure men.”
+     (Proverbs 22:29)""")
     
 
 proverbs_22_29()
